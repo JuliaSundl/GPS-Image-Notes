ee3979d056d41b83f7eccacb1fccb1d1b80ed8bf
Julia <julia.sundl@edu.fh-wien.ac.at>
2024-12-15 19:06:40 +0100

    Small Adaptions and new APK

--- app/src/main/java/com/example/gps_image_notes/MainActivity.kt
+++ app/src/main/java/com/example/gps_image_notes/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.example.gps_image_notes
 
-import android.animation.ObjectAnimator
 import android.content.Intent
-import android.media.Image
 import android.os.Bundle
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,11 +18,6 @@ class MainActivity : AppCompatActivity() {
 
         // Animate/Rotate Icon
         val icon = findViewById<ImageView>(R.id.icon)
-
-        // Rotate Once
-//        val rotateAnimator = ObjectAnimator.ofFloat(icon, "rotation", 0f, 360f)
-//        rotateAnimator.duration = 1000
-//        rotateAnimator.start()
 
         // Smooth Infinite Rotation
         val rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_infinite)
